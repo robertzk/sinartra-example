562fbeaee53fb073103c9a30d52a02c93f1f267c
RobertZK <technoguyrob@gmail.com>
2015-10-02 09:57:11 -0500

    Remove setup.R

--- setup.R
+++
@@ -1,9 +0,0 @@
-# Download mutatr and sinartra
-
-if (!require("devtools")) {
-  install.packages("devtools")
-}
-
-devtools::install_github("hadley/mutatr")
-devtools::install_github("hadley/sinartra")
-
