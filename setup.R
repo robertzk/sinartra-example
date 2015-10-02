044d408473dd7b014fca3e6ae371abcf003cacb2
RobertZK <technoguyrob@gmail.com>
2015-10-02 09:32:37 -0500

    Add installation script

---
+++ setup.R
@@ -0,0 +1,9 @@
+# Download mutatr and sinartra
+
+if (!require("devtools")) {
+  install.packages("devtools")
+}
+
+devtools::install_github("hadley/mutatr")
+devtools::install_github("hadley/sinartra")
+
